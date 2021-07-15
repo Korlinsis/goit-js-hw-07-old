9c1ef11a1046dfd04cc33a77fea0dedb5c9452c3
Ivan Kiziuk <ivanmnksos@gmail.com>
2021-07-15 17:38:31 +0300

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,11 @@
+const inputField = document.querySelector('#validation-input');
+
+inputField.addEventListener('blur', onBorderChangeInput);
+
+function onBorderChangeInput() {
+    if (inputField.value.length === input.data-length) {
+         nameElement.textContent = defaultName;
+          } else {
+        
+    };
+};
